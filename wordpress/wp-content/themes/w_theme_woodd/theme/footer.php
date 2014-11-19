c3afd63c8d35a35455cf24097f8b8e3cde70cc00
michou <michael.bontyes@edelman.com>
2014-11-18 22:19:09 -0500

    footer update

--- wordpress/wp-content/themes/w_theme_woodd/theme/footer.php
+++ wordpress/wp-content/themes/w_theme_woodd/theme/footer.php
@@ -23,14 +23,14 @@
 
 				<p class="credit">
 					<?php
-					printf(
+					// printf(
 						// Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link.
 						// __( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'w_theme' ),
 						// date_i18n( 'Y' ),
 						// hybrid_get_site_link(),
 						// hybrid_get_wp_link(),
 						// hybrid_get_theme_link()
-					);
+					// );
 					?>
 				</p><!-- .credit -->
 
